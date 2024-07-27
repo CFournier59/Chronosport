04f52b47aa89b43debbf669b3bf9c5bab9efe282
CFournier59 <clementfournier.c@gmail.com>
2024-07-27 10:37:08 +0200

    sound added

--- script.js
+++ script.js
@@ -13,10 +13,16 @@ function exCount(){
     let timer = setInterval(function(){
         exTime.innerHTML = sec
         sec --
+        if(sec < 7  && sec > -1){
+            let timeOutBeep = new Audio("sounds/timeOutBeep.mp3")
+            timeOutBeep.play()
+        }
         if (sec < 0){
+            let reposBeep = new Audio("sounds/reposBeep.mp3")
+            reposBeep.play()
             clearInterval(timer)
             reCount()
-        }
+        }    
     }, 1000)
 }
 function reCount(){
@@ -27,11 +33,22 @@ function reCount(){
     let timer = setInterval(function(){
         reTime.innerHTML = sec
         sec --
+        if(sec < 7  && sec > -1){
+            let timeOutBeep = new Audio("sounds/timeOutBeep.mp3")
+            timeOutBeep.play()
+        }
         if(sec < 0){
             clearInterval(timer)
             nbCycles.innerHTML ++
             if(nbCycles.innerHTML <= cyVal){
+                let exerciceBeep = new Audio("sounds/exerciceBeep.mp3")
+                exerciceBeep.play()
                 exCount()
+            }
+            else{
+                nbCycles.innerHTML = 0
+                let finishBeep = new Audio("sounds/finishBeep.mp3")
+                finishBeep.play()
             } 
         }
     }, 1000)
@@ -42,6 +59,8 @@ button.addEventListener("click", () =>{
     let cyVal = Number(cycles.value)
     nbCycles.innerHTML = 1
     if((exVal >= 1) && (reVal >= 1) && (cyVal >=1)){
+        let exerciceBeep = new Audio("sounds/exerciceBeep.mp3")
+        exerciceBeep.play()
         exCount() 
     }
     else{
